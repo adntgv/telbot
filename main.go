bc49def6b5874d512e33c01dda12dc68a63a4fdb
Aidyn Torgayev <at@cybersec.kz>
2018-01-23 20:03:40 +0600

    add conversion

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"net/http"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"os/exec"
 
 	"gopkg.in/telegram-bot-api.v4"
 )
@@ -13,9 +16,7 @@ func main() {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	bot.Debug = false
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -25,22 +26,35 @@ func main() {
 	for update := range updates {
 		if update.Message.Voice == nil {
 			continue
-		} 
+		}
 		var f tgbotapi.FileConfig
 		f.FileID = update.Message.Voice.FileID
 		file, err := bot.GetFile(f)
-		if err != nil{
-			log.Printf("%s",err)
+		if err != nil {
+			log.Printf("%s", err)
 		}
 		resp, err := http.Get(file.FilePath)
 		if err != nil {
-			log.Printf("%s",err)
+			log.Printf("%s", err)
 		}
 		defer resp.Body.Close()
-		
+		ogg, err := os.Create("mes.ogg")
+		if err != nil {
+			log.Printf("%s", err)
+		}
+
+		_, err = io.Copy(ogg, resp.Body)
+		if err != nil {
+			log.Printf("%s", err)
+		}
+		cmd := exec.Command("soundconverter", "mes.ogg", "-b", "-s", ".wav", "-m", "wav")
+		err = cmd.Run()
+		if err != nil {
+			log.Printf("%s", err)
+		}
+
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 		msg.ReplyToMessageID = update.Message.MessageID
-
 		bot.Send(msg)
 	}
 }
