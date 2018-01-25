91daf847d9f22bb1e3dc8698b2e389f82c4d3e15
Aidyn Torgayev <right-hearted@yandex.ru>
2018-01-26 02:23:25 +0600

    use ffmpeg for audio conversion

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 		if err != nil {
 			log.Printf("%s", err)
 		}
-		cmd := exec.Command("soundconverter", "mes.ogg", "-b", "-s", ".wav", "-m", "wav")
+		cmd := exec.Command("ffmpeg","-i", "mes.ogg", "mes.wav")
 		err = cmd.Run()
 		if err != nil {
 			log.Printf("%s", err)
